799bb3bb4437bb445f683d42d82bb562d78c7602
alanchen1218 <alanchen1218@gmail.com>
2020-06-25 08:35:32 -0700

    adding favorite/retweet feature

---
+++ bots/favretweet.py
@@ -0,0 +1,12 @@
+import tweepy
+import logging
+from config import create_api
+import json
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+class FavRetweetListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
