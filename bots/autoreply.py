a89f0deed87ed0dc166dbd92af95b06ed4eb29fb
alanchen1218 <alanchen1218@gmail.com>
2020-06-26 16:41:58 -0700

    starting to add auto reply feature

---
+++ bots/autoreply.py
@@ -0,0 +1,11 @@
+import tweepy
+import logging
+from config import create_api
+import time
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+def check_mentions(api, keywords, since_id):
+    logger.info("Retrieving mentions")
+    new_since_id = since_id
